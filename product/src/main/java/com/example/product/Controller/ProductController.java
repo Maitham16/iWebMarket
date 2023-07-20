f5f9444fa1109fa383e390bc0927a00f4c3bf156
Maitham Al-rubaye <maythem.altamar@gmail.com>
2023-07-20 22:51:34 +0200

    simple front-end

--- product/src/main/java/com/example/product/Controller/ProductController.java
+++ product/src/main/java/com/example/product/Controller/ProductController.java
@@ -2,13 +2,13 @@ package com.example.product.Controller;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.product.Service.ProductService;
 import com.example.product.dto.ProductRequest;
@@ -21,45 +21,55 @@ import lombok.AllArgsConstructor;
  * @author maith
  */
 
-@RestController
+@Controller
 @RequestMapping("/api/product")
 @AllArgsConstructor
 public class ProductController {
 
-    final private ProductService productService;
+    private final ProductService productService;
 
-    // Create a new product
+    @GetMapping("/create")
+    public String createProductForm(Model model) {
+        model.addAttribute("product", new ProductRequest());
+        return "product-form";
+    }
+
     @PostMapping("/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct(@RequestBody ProductRequest productRequest) {
+    public String createProduct(@ModelAttribute ProductRequest productRequest) {
         productService.createProduct(productRequest);
+        return "redirect:/api/product/all";
     }
 
-    // Get all products
     @GetMapping("/all")
-    @ResponseStatus(HttpStatus.OK)
-    public List<ProductResponse> getAllProducts() {
-        return productService.getAllProducts();
+    public String getAllProducts(Model model) {
+        List<ProductResponse> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "product-list";
     }
 
-    // update a product
-    @PostMapping("/update")
-    @ResponseStatus(HttpStatus.OK)
-    public void updateProduct(String id, @RequestBody ProductRequest productRequest) {
+    @GetMapping("/update/{id}")
+    public String showUpdateForm(@PathVariable String id, Model model) {
+        ProductResponse product = productService.getProduct(id);
+        model.addAttribute("product", product);
+        return "product-update-form";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateProduct(@PathVariable String id, @ModelAttribute ProductRequest productRequest) {
         productService.updateProduct(id, productRequest);
+        return "redirect:/api/product/all";
     }
 
-    // delete a product
-    @PostMapping("/delete")
-    @ResponseStatus(HttpStatus.OK)
-    public void deleteProduct(String id){
+    @PostMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable String id) {
         productService.deleteProduct(id);
+        return "redirect:/api/product/all";
     }
 
-    // get a product by id
-    @GetMapping("/get")
-    @ResponseStatus(HttpStatus.OK)
-    public ProductResponse getProduct(String id){
-        return productService.getProduct(id);
+    @GetMapping("/view/{id}")
+    public String getProduct(@PathVariable String id, Model model) {
+        ProductResponse product = productService.getProduct(id);
+        model.addAttribute("product", product);
+        return "product-view";
     }
 }
--- product/src/test/java/com/example/product/ProductApplicationTests.java
+++ product/src/test/java/com/example/product/ProductApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,11 +12,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
 import com.example.product.Repository.ProductRepository;
-import com.example.product.dto.ProductRequest;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -30,10 +27,6 @@ class ProductApplicationTests {
 	// MockMvc
 	@Autowired
 	private MockMvc mockMvc;
-
-	// object mapper
-	@Autowired
-	private ObjectMapper objectMapper;
 
 	// product repository
 	@Autowired
@@ -51,22 +44,15 @@ class ProductApplicationTests {
 
 	@Test
 	void shouldCreateProduct() throws Exception {
-		ProductRequest productRequest = getProductRequest();
-		String productRequestString = objectMapper.writeValueAsString(productRequest);
 		mockMvc.perform(MockMvcRequestBuilders.post("/api/product/create")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(productRequestString))
-				.andExpect(status().isCreated());
+				.param("name", "Item 1")
+				.param("description", "description description description description description description description description description description description description description description description description description description description description description")
+				.param("price", "1200")
+				.param("image", "link")
+				.param("category", "Category 1"))
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/api/product/all"));
 		Assertions.assertEquals(1, productRepository.findAll().size());
 	}
-
-	private ProductRequest getProductRequest() {
-		return ProductRequest.builder()
-				.name("Item 1")
-				.description("description description description description description description description description description description description description description description description description description description description description description  ")
-				.price(1200)
-				.image("link")
-				.category("Category 1")
-				.build();
-	}
+	
 }
